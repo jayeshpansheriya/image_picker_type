d8a6906fe3908bdb48a28b53c19d6825c5ce8b97
jayeshpansheriya <pansheriyajayesh@gmail.com>
2020-07-18 17:07:51 +0530

    add code

--- lib/flutter_image_picker.dart
+++ lib/flutter_image_picker.dart
@@ -1,7 +1,88 @@
 library flutter_image_picker;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+enum ImagePickerType { GALLERY, CAMERA }
+
+class ImagePickerHelper extends StatelessWidget {
+
+  const ImagePickerHelper({Key key, this.onDone, this.isCropped, this.size})
+      : super(key: key);
+
+  final Function(File) onDone;
+  final bool isCropped;
+  final Size size;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new ListTile(
+            title: new Text('Gallery '),
+            onTap: () async {
+              getCroppedImage(ImagePickerType.GALLERY, size.height, size.width)
+                  .then((img) {
+                Navigator.pop(context);
+                onDone(img);
+              });
+            },
+          ),
+          new ListTile(
+            title: new Text('Camera'),
+            onTap: () async {
+              getCroppedImage(ImagePickerType.CAMERA, size.height, size.width)
+                  .then((img) {
+                Navigator.pop(context);
+                onDone(img);
+              });
+            },
+          ),
+          new ListTile(
+            title: new Text('Cancel', style: new TextStyle(color: Colors.red)),
+            onTap: () {
+              Navigator.pop(context);
+              onDone(null);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<File> getCroppedImage(
+      ImagePickerType type, double height, double width) async {
+    final picker = ImagePicker();
+
+    return picker.getImage(
+        source: type == ImagePickerType.CAMERA
+            ? ImageSource.camera
+            : ImageSource.gallery)
+        .then((img) {
+      return ImageCropper.cropImage(
+          sourcePath: img.path,
+          maxHeight: height.toInt(),
+          maxWidth: width.toInt(),
+          aspectRatio: CropAspectRatio(
+              ratioX: height.toDouble(), ratioY: width.toDouble()),
+          aspectRatioPresets: [
+            CropAspectRatioPreset.square,
+          ],
+          androidUiSettings: AndroidUiSettings(
+              toolbarTitle: 'Cropper',
+              toolbarColor: Colors.deepOrange,
+              toolbarWidgetColor: Colors.white,
+              initAspectRatio: CropAspectRatioPreset.original,
+              lockAspectRatio: false),
+          iosUiSettings: IOSUiSettings(
+            minimumAspectRatio: 1.0,
+          ));
+    });
+  }
 }
